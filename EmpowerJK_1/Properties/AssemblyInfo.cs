7d04c3453975d859f7d55da257188ef75e901148
joerg-knesebeck_nttltd <joerg.knesebeck@global.ntt>
2023-03-28 14:24:50 +0200

    Add project files.

---
+++ EmpowerJK_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerJK_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerJK_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("CB742243-957B-4339-A375-40C7913D924C")]
+[assembly: AssemblyVersion("1.0.0.0")]
